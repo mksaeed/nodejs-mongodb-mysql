01af1cb56eef0659516a63c9936a6a81bef0e960
Muhammad Kamran Saeed <kamran.muhammad@outlook.com>
2017-03-24 17:39:29 +0500

    ReUploading Data

---
+++ database/mysql-connect.js
@@ -0,0 +1,60 @@
+const mysql = require('mysql');
+
+// const connection = mysql.createConnection('mysql://root:@localhost/node_dbdb?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
+
+const connection = mysql.createConnection({
+    host:   'localhost',
+    user:   'root',
+    password: '',
+    database : 'node_db'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.log('Unable to connect to mysql Server');
+  }
+ 
+  console.log('connected to mysql Server');
+
+
+// //Inserting Data into Mysql Database Users Table
+//   var post = {
+//       id: '',
+//       name: 'Kamran Saeed',
+//       password: 'abc123',
+//       status: 'Active'
+
+//   };
+
+//   var query = connection.query('INSERT INTO user SET ?', post, function (error, results, fields) {
+//        if (error) throw error;
+//   // Neat! 
+// });
+// console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL' 
+ 
+
+ //Fetching Data From Mysql Database Users Table
+
+ var string = 'SELECT * From user';
+
+ var res = connection.query(string, function(err, results, fields){
+     if(err){
+         return console.log('Unable To Fetch Data From Table');
+     }
+//      for (var i in results){
+//      console.log(results[i]);
+//  }
+    console.log(results[1]);
+    console.log(fields[3]);
+
+    // error will be an Error if one occurred during the query 
+    // results will contain the results of the query 
+    // fields will contain information about the returned results fields (if any) 
+  }
+);
+ });
+
+
+
+  connection.end();
+});
